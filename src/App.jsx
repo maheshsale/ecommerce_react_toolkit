a0c65247a8e730ab7f88cdbca9f4a17ba49b6ad5
maheshsale <maheshsale333.com>
2024-11-11 14:08:44 -0500

    Completed Lab

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,16 @@
 import React from 'react';
 import ProductList from './Components/ProductList';
 import ShoppingCart from './Components/ShoppingCart';
+import SuperCoins from './Components/SuperCoins'
 import './App.css'
+
 const App = () => {
   return (
       <div>
         <h1 className='app-heading'>E-Commerce Application</h1>
         <ProductList />
         <ShoppingCart />
+        <SuperCoins />
       </div>
 
   );
